5f9b89ba5cfc73eb7f02b47c4e12cd63c57b0768
Nga Nguyen <nganguyen.ny@gmail.com>
2021-04-19 18:16:01 +0700

    web scrap top 100 movies Empire

---
+++ project-day-45-webscrap-100-movies/main.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+contents = response.text
+
+soup = BeautifulSoup(contents, "html.parser")
+
+movie_list = [movie.img.get("alt") for movie in soup.find_all(class_="listicle-item")]
+
+with open("movies.txt", mode="a") as file:
+    order = 1
+    for movie in list(reversed(movie_list)):
+        file.write(f"{order}) {movie}\n")
+        order += 1
