88247875e3aa45fef940bf4a14b1f10e12c3cde6
Nga Nguyen <nganguyen.ny@gmail.com>
2021-06-29 21:18:50 +0700

    Add comments for each step

--- project-day-48-selenium-webdriver-game-playing-bot/main.py
+++ project-day-48-selenium-webdriver-game-playing-bot/main.py
@@ -1,44 +1,45 @@
 from selenium import webdriver
 import time
 
-GAME_DURATION_MIN = 5
-BUY_NEW_ITEM_SEC = 5
-
 chrome_driver_path = "/Users/nganguyen/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
+# Get cookie to click on.
 cookie = driver.find_element_by_id("cookie")
 
-timeout = time.time() + 60*GAME_DURATION_MIN
-time_to_buy = time.time() + BUY_NEW_ITEM_SEC
+timeout = time.time() + 60*5  # 5 minutes
+time_to_buy = time.time() + 5
 
-game_is_on = True
-while game_is_on:
+while True:
 
     cookie.click()
 
+    # After 5 minutes stop the bot and check the cookies per second count.
     if time.time() > timeout:
-        cps = driver.find_element_by_id("cps").text
-        print(cps)
+        cookie_per_second = driver.find_element_by_id("cps").text
+        print(cookie_per_second)
         break
 
+    # Every 5 seconds:
     if time.time() > time_to_buy:
+        # Get current cookie count
         money = int(driver.find_element_by_id("money").text.replace(",", ""))
 
-        upgrades = driver.find_elements_by_css_selector("#store b")
-        # The following line can lead to a selenium.common.exceptions.StaleElementReferenceException Error if
-        # time_to_buy is too small
-        store = {up.text.split(" - ")[0]: int(up.text.split(" - ")[1].replace(",", "")) for up in upgrades[:-1]}
+        # Create dictionary of store items and prices
+        store = driver.find_elements_by_css_selector("#store b")
+        upgrades = {item.text.split(" - ")[0]: int(item.text.split(" - ")[1].replace(",", "")) for item in store[:-1]}
 
-        most_expensive_affordable = [(item, cost) for (item, cost) in store.items() if cost < money]
+        # Find upgrades that we can currently afford
+        most_expensive_affordable = [(item, cost) for (item, cost) in upgrades.items() if cost < money]
 
+        # Purchase the most expensive affordable upgrade
         if most_expensive_affordable:
-            # print(most_expensive_affordable[-1][0])
             buy = driver.find_element_by_id(f"buy{most_expensive_affordable[-1][0]}")
             buy.click()
 
-        time_to_buy += BUY_NEW_ITEM_SEC
+        # Add another 5 seconds until the next check
+        time_to_buy += 5
 
 driver.quit()
