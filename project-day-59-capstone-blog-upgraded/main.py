16661c8f58581078377a79fcc664ed61b40629f2
Nga Nguyen <nganguyen.ny@gmail.com>
2021-07-22 13:55:39 +0700

    POST request with flask

--- project-day-59-capstone-blog-upgraded/main.py
+++ project-day-59-capstone-blog-upgraded/main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+from account_info import my_email, my_password, recipient
+import smtplib
 
 app = Flask(__name__)
 response = requests.get("https://api.npoint.io/e42b353ee387383898c7")
@@ -23,11 +25,24 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data['name'], data['email'], data['phone'], data['message'])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=recipient,
+                            msg=f"Subject: New message on your blog"
+                                f"\n\n{name}\n{email}\n{phone}\n{message}")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
