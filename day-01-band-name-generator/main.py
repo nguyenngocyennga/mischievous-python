fefdd4b43b70b973f3bd90fa6c86d44134eebf2f
Nguyen Ngoc Yen Nga <nganguyen.ny@gmail.com>
2023-03-24 19:25:26 +0700

    Working with Variables in Python to Manage Data

--- day-01-band-name-generator/main.py
+++ day-01-band-name-generator/main.py
@@ -1,3 +1,5 @@
+# Working with Variables in Python to Manage Data
+
 #1. Create a greeting for your program.
 #2. Ask the user for the city that they grew up in.
 #3. Ask the user for the name of a pet.
