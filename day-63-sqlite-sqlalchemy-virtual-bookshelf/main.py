1bc5952b95329dba1eadf3e3b0caa8974e19f1c7
Nguyen Ngoc Yen Nga <nganguyen.ny@gmail.com>
2023-03-24 20:08:24 +0700

    Databases and with SQLite and SQLAlchemy

--- day-63-sqlite-sqlalchemy-virtual-bookshelf/main.py
+++ day-63-sqlite-sqlalchemy-virtual-bookshelf/main.py
@@ -1,3 +1,5 @@
+# Databases and with SQLite and SQLAlchemy
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from typing import Callable
